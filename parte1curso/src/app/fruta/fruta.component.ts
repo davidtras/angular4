701224ff9b4f96210df60cb25a974a5a09c2f012
david.molina <david.molina@p-externos.iecisa.com>
2017-10-06 14:56:58 +0200

    metodo ngOnInit

--- parte1curso/src/app/fruta/fruta.component.ts
+++ parte1curso/src/app/fruta/fruta.component.ts
@@ -8,4 +8,34 @@ import {Component} from '@angular/core';
 export class FrutaComponent{
     public nombre_componente='Componente de fruta';
     public listado_frutas='Naranja,Manzana,Pera y Sandia';
+
+    public nombre:string;
+    public edad:number;
+    public mayorDeEdad:boolean;
+    public trabajos:Array<any>=['Carpintero',44,'Fontanero'];
+    public comodin:any;
+
+    constructor(){
+        this.nombre= 'David Molina';
+        this.edad = 77;
+        this.mayorDeEdad = false;
+        this.comodin= 'SI';
+
+    }
+
+    ngOnInit(){
+
+        this.cambiarNombre();
+        this.cambiarEdad(23);
+        console.log(this.nombre + " " + this.edad);
+    }
+
+    cambiarNombre(){
+        this.nombre = 'Angel López';
+    }
+
+    cambiarEdad(edad){
+        this.edad = edad;
+    }
+
 }
