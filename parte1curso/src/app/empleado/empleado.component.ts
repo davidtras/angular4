ff574ce92af19a8c24bc4334fe684a34c3a6f236
davidtras <davidtras@hotmail.com>
2017-10-09 22:31:36 +0200

    Directivas

--- parte1curso/src/app/empleado/empleado.component.ts
+++ parte1curso/src/app/empleado/empleado.component.ts
@@ -1,10 +1,34 @@
 import {Component} from '@angular/core';
-
+import {Empleado} from './empleado';
 
 @Component({
     selector: 'empleado-tag',
     templateUrl: './empleado.component.html'
 })
 export class EmpleadoComponent{
-    public titulo = 'Titulo del componente empleado';
+    public titulo = 'Componente Empleado';
+    public empleado:Empleado;
+    public trabajadores:Array<Empleado>;
+    public trabajador_externo:boolean;
+    public color:string;
+
+    constructor(){
+        this.empleado = new Empleado('Antonio Cadenas',31,'Cocinero',true);
+        this.trabajadores = [
+            new Empleado('Antonio Cadenas',31,'Cocinero',true),
+            new Empleado('Pedro Guinche',33,'Gañán',false),
+            new Empleado('Silvia Pérez',32,'Azafata',true),
+            new Empleado('Roberto Sedinho',65,'Entrenador',false)];
+        this.trabajador_externo = false;
+        this.color='green';
+    }
+
+    ngOnInit(){
+        console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
+
+    cambiarExterno(valor){
+        this.trabajador_externo = valor;
+    }
 }
