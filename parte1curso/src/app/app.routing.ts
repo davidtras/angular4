524aaf64e2345cbd500054dcc8203f1ae244439d
davidtras <davidtras@hotmail.com>
2017-10-16 22:36:10 +0200

    rutas

--- parte1curso/src/app/app.routing.ts
+++ parte1curso/src/app/app.routing.ts
@@ -3,12 +3,16 @@ import {Routes,RouterModule} from '@angular/router';
 
 import {EmpleadoComponent} from './empleado/empleado.component';
 import {FrutaComponent} from './fruta/fruta.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 const appRoutes : Routes = [
-    {path: '', component: EmpleadoComponent},
+    {path: '', component: HomeComponent},
     {path: 'empleado', component: EmpleadoComponent},
     {path: 'fruta', component: FrutaComponent},
-    {path: '**', component: EmpleadoComponent}
+    {path: 'pagina-principal', component: HomeComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: '**', component: HomeComponent}
 ];
 
 export const appRoutingProviders:any[] = [];
