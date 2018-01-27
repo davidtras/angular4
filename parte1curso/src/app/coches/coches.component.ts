a8e689ed1d65da64c33343e28f24088abe06fc0c
davidtras <davidtras@hotmail.com>
2018-01-27 11:08:46 +0100

    d

--- parte1curso/src/app/coches/coches.component.ts
+++ parte1curso/src/app/coches/coches.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import {Coche} from './coche';
+import {PeticionesService} from '../services/peticiones.service';
     
 @Component({
     selector: 'coches',
-    templateUrl: './coches.component.html'
+    templateUrl: './coches.component.html',
+    providers: [PeticionesService]
 })
 export class CochesComponent{
 
     public coche: Coche;
-
-    constructor(){
+    public coches:Array<Coche>;
+l
+    constructor(
+        private _service:PeticionesService
+    ){
         this.coche = new Coche("","","");
+        this.coches = [
+            new Coche("Seat Panda","105","Azul"),
+            new Coche("Renault Megane","175","Rojo")
+        ];
+    }
+
+    ngOnInit(){
+        alert(this._service.getPrueba());
     }
 
     onSubmit(){
